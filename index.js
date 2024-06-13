db9a826c6b090c6d8396a871db15b6a711757027
Antu Chandra Das <antu.das.619@gmail.com>
2024-06-14 01:08:27 +0600

    Verify all route with jwt and

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+var jwt = require('jsonwebtoken');
 const app = express();
 const port = 5000;
 
@@ -18,15 +19,43 @@ const client = new MongoClient(uri, {
   },
 });
 
+// jwt verify middleware
+function verifyJWT(req, res, next){
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send('unauthorized access')
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.TOKEN_SECRET, function(error, decoded){
+    if(error){
+      return res.status(403).send({message: 'forbidden access'})
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
+
 async function run() {
   try {
-    await client.connect();
     const events = client.db("events");
     const bookings = client.db("bookings");
     const users = client.db("users");
     const eventCollection = events.collection("eventCollection");
     const bookingsCollection = bookings.collection("bookingsCollection");
     const userCollection = users.collection("userCollection");
+
+    // JWT
+    app.get('/jwt', async(req, res) => {
+      const email = req.query.email;
+      const query = {email}
+      const user = await userCollection.findOne(query);
+      if(user){
+        const token = jwt.sign({email}, process.env.TOKEN_SECRET, {expiresIn: '1h'})
+        return res.send({token: token})
+      }
+      res.status(403).send({token: 'token not found'})
+    })
 
     // Get all events
     app.get("/events", async (req, res) => {
@@ -36,29 +65,33 @@ async function run() {
     });
 
     // Get single event
-    app.get("/events/:id", async (req, res) => {
+    app.get("/events/:id", verifyJWT, async (req, res) => {
       const id = req.params.id;
       const event = await eventCollection.findOne({ _id: new ObjectId(id) });
       res.send(event);
     });
 
     // get bookings
-    app.get("/bookings", async (req, res) => {
+    app.get("/bookings",verifyJWT, async (req, res) => {
       const email = req.query.email;
+      const decodedEmail = req.decoded.email;
+      if(email !== decodedEmail){
+        return res.status(403).send({message: 'forbidden access'})
+      }
       const query = { email: email };
       const result = await bookingsCollection.find(query).toArray();
       res.send(result.reverse());
     });
 
     // post bookings
-    app.post("/bookings", async (req, res) => {
+    app.post("/bookings", verifyJWT, async (req, res) => {
       const booking = req.body;
       const result = await bookingsCollection.insertOne(booking);
       res.send(result);
     });
 
     // Update event available tickets
-    app.patch("/events/:id", async (req, res) => {
+    app.patch("/events/:id", verifyJWT, async (req, res) => {
       const id = req.params.id;
       const updatedDoc = req.body;
       const data = await eventCollection.updateOne(
@@ -83,7 +116,7 @@ async function run() {
     })
 
     // get user from db
-    app.get('/users', async(req, res) => {
+    app.get('/users',verifyJWT, async(req, res) => {
       const user = userCollection.find();
       const result = await user.toArray(user);
       res.send(result);
